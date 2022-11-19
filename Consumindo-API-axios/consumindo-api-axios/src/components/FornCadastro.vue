55ae8608a4ba61d395614f9b20c1b6773cadcf5f
EdivanSanto5 <capic20191tads0326@aluno.ifpi.edu.br>
2022-11-19 11:45:11 -0300

    finalizando modificações

--- Consumindo-API-axios/consumindo-api-axios/src/components/FornCadastro.vue
+++ Consumindo-API-axios/consumindo-api-axios/src/components/FornCadastro.vue
@@ -1,13 +1,74 @@
-<template >
+
+<template>
+    <div id="app">
+  
+      <nav>
+        <div class="nav-wrapper blue darken-1">
+          <a href="#" class="brand-logo center">Produtos Front</a>
+        </div>
+      </nav>
+  
+      <div class="container">
+  
+        <form>
+  
+            <!-- <label>Nome</label> -->
+            <input type="text" placeholder="Nome do Produto">
+            <!-- <label>Quantidade</label> -->
+            <input type="number" placeholder="Quantidade de Produtos">
+            <!-- <label>Valor</label> -->
+            <input type="text" placeholder="Valor do ptoduto">
+  
+            <button class="waves-effect waves-light btn-small">Salvar<i class="material-icons left">save</i></button>
+
+        </form>
+  
+        <table>
+        <thead>
+  
+            <tr>
+              <th>NOME</th>
+              <th>QUANTIDADE</th>
+              <th>VALOR</th>
+              <th>OPÇÕES</th>
+            </tr>
+  
+        </thead>
+  
+          <tbody>
+  
+            <tr>
+  
+              <td>Arduino</td>
+              <td>100</td>
+              <td>50.00</td>
+              <td>
+                <button class="waves-effect btn-small blue darken-1"><i class="material-icons">create</i>Editar</button>
+                <button class="waves-effect btn-small red darken-1"><i class="material-icons">delete_sweep</i>Deletar</button>
+              </td>
+  
+            </tr>
+  
+          </tbody>
+        
+        </table>
+  
+      </div>
+  
+    </div>
+  </template>
+  
+  <!-- <template >
     <div id="app">
         <nav>
-             <div class="nav-wrapper blue darken-1"> <!-- aqui vai fazer a tela de menu com as cores azul -->
+             <div class="nav-wrapper blue darken-1"> 
                 <a href="#" class="brand-log center ">Produtos do Front-end</a> 
             </div>
         </nav>
-        <div class="conteiner">
+        <nav>
+        
+        <div class="container">
 
-            <!-- formulario -->
             <form>
                 <label>Nome</label>
                 <input type="text" placeholder="Nome do Produto">
@@ -16,11 +77,10 @@
                 <label>Valor</label>
                 <input type="text" placeholder="Valor do produto">
 
-                <button class="< waves-effect waves-light btn-small">Salvar <i class="material-icons left">save</i></button> <!--<i class="material-icons left">save</i> vai add icone de salvar -->
-            </form>
-
-            <!--fazendo uma tabela 1 linha e 4 colunas -->
-            <!--<tr> quantidade de linha e <th> quantidade de colunas-->
+                 <button class="< waves-effect waves-light btn-small">Salvar <i class="material-icons left">save</i></button>
+                
+            </form> 
+            
             <thead>
                 <tr> 
                     <th>HOME</th>
@@ -29,18 +89,22 @@
                     <th>OPÇÕES</th>
                 </tr>
             </thead>
+
+            <thead>
+  
             <tbody>
                 <tr>
                     <td>Arduino</td>
                     <td>45</td>
                     <td>25000</td>
                     <td>
-                        <button class="aves-effect btn-small blue darken-1"><i class="material-icons">create</i>Editar</button>
-                        <button class="aves-effect btn-small red darken-1"><i class="material-icons">delete_sweepp</i>Deletar</button>
+                        <button class="waves-effect btn-small blue darken-1"><i class="material-icons">create</i>Editar</button>
+                        <button class="waves-effect btn-small red darken-1"><i class="material-icons">delete_sweepp</i>Deletar</button>
                     </td>
                     
                 </tr>
             </tbody>
+            
         </div>
 
     </div>
@@ -50,4 +114,4 @@
 export default {
     name:'FornCadastro',
 }
-</script>
+</script> -->
