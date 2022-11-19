3202040018db6a2715c5ab26db0932abb1aaf268
EdivanSanto5 <capic20191tads0326@aluno.ifpi.edu.br>
2022-11-19 09:37:24 -0300

    criando a estrutura do mini projeto e fazendo um formulario com uma pequena tabela

---
+++ Consumindo-API-axios/consumindo-api-axios/src/components/FornCadastro.vue
@@ -0,0 +1,29 @@
+<template >
+    <div id="app">
+        <nav>
+             <div class="nav-wrapper blue darken-1"> <!-- aqui vai fazer a tela de menu com as cores azul -->
+                <a href="#" class="brand-log center ">Produtos do Front-end</a> 
+            </div>
+        </nav>
+        <div class="conteiner">
+            <form>
+
+            </form>
+            <thead>
+                <tr>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                </tr>
+            </thead>
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name:'FornCadastro',
+}
+</script>
