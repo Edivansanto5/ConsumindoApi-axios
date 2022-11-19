db8106be93370a444bcefb8c7730034603cfd251
EdivanSanto5 <capic20191tads0326@aluno.ifpi.edu.br>
2022-11-19 10:28:14 -0300

    criando uma tabela para exibir os pprodutos

--- Consumindo-API-axios/consumindo-api-axios/src/components/FornCadastro.vue
+++ Consumindo-API-axios/consumindo-api-axios/src/components/FornCadastro.vue
@@ -15,7 +15,8 @@
                 <input type="number" placeholder="Qauntidade de produtos">
                 <label>Valor</label>
                 <input type="text" placeholder="Valor do produto">
-                 <button class="< waves-effect waves-light btn-small">Salvar <i class="material-icons left">save</i></button> <!--<i class="material-icons left">save</i> vai add icone de salvar -->
+
+                <button class="< waves-effect waves-light btn-small">Salvar <i class="material-icons left">save</i></button> <!--<i class="material-icons left">save</i> vai add icone de salvar -->
             </form>
 
             <!--fazendo uma tabela 1 linha e 4 colunas -->
@@ -28,6 +29,18 @@
                     <th>OPÇÕES</th>
                 </tr>
             </thead>
+            <tbody>
+                <tr>
+                    <td>Arduino</td>
+                    <td>45</td>
+                    <td>25000</td>
+                    <td>
+                        <button class="aves-effect btn-small blue darken-1"><i class="material-icons">create</i>Editar</button>
+                        <button class="aves-effect btn-small red darken-1"><i class="material-icons">delete_sweepp</i>Deletar</button>
+                    </td>
+                    
+                </tr>
+            </tbody>
         </div>
 
     </div>
